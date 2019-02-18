5bdace1b91517e33ff3c32cd1854d01349b8bef9
LFernandoTorres <lftorresgarces@gmail.com>
2019-02-17 23:54:35 -0500

    Backend Login index

--- backend/index.php
+++ backend/index.php
@@ -4,14 +4,16 @@
   <meta charset="UTF-8">
   <title>Login</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" href="css/login.css">
+
 </head>
-<body class="text-center">
-  <form class="form-signin">
-  <img class="mb-4" src="../img/logotipo.png" alt="" width="72" height="72">
-  <h1 class="h3 mb-3 font-weight-normal">Iniciar Sesion</h1>
-  <label for="inputEmail" class="sr-only">Correo Electonico</label>
-  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+<body>
+   <body class="text-center">
+    <form class="form-signin">
+  <img class="mb-4" src="img/logotipo.png">
+  <h1 id="text1" class="h3 mb-3 font-weight-normal">Inicio Sesion</h1>
+  <label for="inputEmail" class="sr-only">Correo Electronico</label>
+  <input type="email" id="inputEmail" class="form-control" placeholder="Dirección de correo" required autofocus>
   <label for="inputPassword" class="sr-only">Password</label>
   <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
   <div class="checkbox mb-3">
@@ -19,12 +21,13 @@
       <input type="checkbox" value="remember-me"> Recuerdame
     </label>
   </div>
-  <button class="btn btn-lg btn-primary btn-block" type="button" id="buttonSing">Sign in</button>
+  <button class="btn btn-lg btn-primary btn-block" id= "buttonSign" type="button">Iniciar</button>
   <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
 </form>
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>  
 <script>
 //Estructura Basica de JQuery $().();
 // $ es un alias llamado jquery, normalente llamado mediante pesitos, se ha de usar la palabra reservada cuando exista otro framework, por lo que se debera usar el comando jquery Dento de parentesis siempre deberan ir selectores, llevando 3 comillas solo el selector del navegador no llevara comillas
@@ -42,30 +45,25 @@
   //   alert("Hola");
   // });
 
-   $("#buttonSing").click(function (){
+    $("#buttonSign").click(function(){
+    let correo = $("#inputEmail").val();
     // Obtener Valor del email
-    let correo = $("inputEmail").val();
+    let pass = $("#inputPassword").val();
     // Obtener el valor de Password
-    let correo = $("inputPassword").val();
-    // TEST: -- aler("Tu correo es: " + correo + "y tu password es:" + password +);
     let obj = {
-      "accion" : "login",
-      "usuario" : correo,
-      "password" : password
+      "action" : "login",
+      "correo" : correo,
+      "password" : pass  
     };
-    $.post("includes/_funciones.php", obj, function);
-        
-            //let obj = {}; Con esto ya se clasifica como objeto; no es optimo pero es una notacion oreintada a objetos, emulando cosas como clases 
-            // "atributo" : "valor",
-            // "atributo" : "valor",
-            // "atributo" : "valor" 
-            // Al final ya no llevara coma
-    // Enviar el validador de Valores
-    // En caso de ser valido, redireccionar a usuarios.php
-    // En caso de no ser valido envair mensaje de error 
-
-
-  });
+    if (correo == "" || pass == "") {
+      //  Si usuario y contraseña estan vacios imprimir = 3 
+       alert("Ingresa Correo y Contraseña [ERROR-03]");
+    } 
+    else{
+    $.post('includes/_funciones.php', obj, function() {});
+    }
+    
+});
 </script>
 </body>
 </html>
